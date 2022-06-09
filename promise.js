1b6b077fa9218b01d81db2a461a5ac5dfead07c8
Yusuf Pratomo <yusufpra91@gmail.com>
2022-06-09 23:08:49 +0700

    commit-1

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const dataIXX = await promiseTheaterIXX();
+    const dataVGC = await promiseTheaterVGC();
+    const allData = [...dataIXX, ...dataVGC];
+    const result = allData.filter((e) => {
+      if (e.hasil === emosi) return e;
+    });
+    return result.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
